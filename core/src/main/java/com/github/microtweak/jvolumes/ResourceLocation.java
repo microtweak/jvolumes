742ba62105ae29dffc594a11572fa3a62d770daa
Marcos Koch Salvador <marcosks0@gmail.com>
2020-02-06 00:35:09 -0300

    Refactored ResourceLocation class

--- core/src/main/java/com/github/microtweak/jvolumes/ResourceLocation.java
+++ core/src/main/java/com/github/microtweak/jvolumes/ResourceLocation.java
@@ -13,38 +13,47 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toMap;
+import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
 import static org.apache.commons.lang3.StringUtils.substringAfter;
 
-@Getter
 public final class ResourceLocation {
 
     public static final String PROTOCOL_SEPARATOR = "://";
     public static final String FILE_EXTENSION_SEPARATOR = ".";
 
-    private String protocol;
+    private URI uri;
 
-    private String volumeName;
-
-    private String path;
-
+    @Getter
     private Map<String, String> parameters;
 
     ResourceLocation(String text) {
-        URI uri = null;
-
-        try {
-            uri = new URI(text);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-
-        protocol = uri.getScheme();
-
-        volumeName = uri.getAuthority();
-
-        path = substringAfter(uri.getPath(), "/");
-
+        uri = parseUri(text);
         parameters = getParameterMap(uri.getQuery());
+    }
+
+    public String getProtocol() {
+        return uri.getScheme();
+    }
+
+    public String getVolumeName() {
+        return defaultIfBlank(uri.getAuthority(), "");
+    }
+
+    public String getPath() {
+        return substringAfter(uri.getPath(), "/");
+    }
+
+    @Override
+    public String toString() {
+        return String.join("", getProtocol(), PROTOCOL_SEPARATOR, getVolumeName(), "/", getPath());
+    }
+
+    private URI parseUri(String text) {
+        try {
+            return new URI(text);
+        } catch (URISyntaxException e) {
+            return ExceptionUtils.rethrow(e);
+        }
     }
 
     private Map<String, String> getParameterMap(String query) {
@@ -54,14 +63,14 @@ public final class ResourceLocation {
 
         Map<String, String> params = Stream.of(query.split("&"))
             .map(param -> param.split("="))
-            .peek(parts -> parts[1] = decode(parts[1]))
+            .peek(parts -> parts[1] = decodeUrl(parts[1]))
             .map(parts -> new AbstractMap.SimpleEntry<>(parts[0], parts[1]))
             .collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
 
         return Collections.unmodifiableMap(params);
     }
 
-    private String decode(String text) {
+    private String decodeUrl(String text) {
         try {
             return URLDecoder.decode(text, "UTF-8");
         } catch (UnsupportedEncodingException e) {
--- core/src/test/java/com/github/microtweak/jvolumes/ResourceLocationTests.java
+++ core/src/test/java/com/github/microtweak/jvolumes/ResourceLocationTests.java
@@ -1,46 +1,97 @@
 package com.github.microtweak.jvolumes;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ResourceLocationTests {
 
     @Test
-    public void parseUri() {
-        ResourceLocation ref = new ResourceLocation("file://tmp/uploads/xyz.jpg");
+    public void parseUriWithoutParams() {
+        final ResourceLocation unixRef = new ResourceLocation("file:///tmp/uploads/xyz.jpg");
 
         assertAll(
-                () -> assertEquals(ref.getProtocol(), "file"),
-                () -> assertEquals(ref.getVolumeName(), "tmp"),
-                () -> assertEquals(ref.getPath(), "uploads/xyz.jpg"),
-                () -> assertTrue(ref.getParameters().isEmpty())
+                () -> assertEquals("file", unixRef.getProtocol()),
+                () -> assertEquals("", unixRef.getVolumeName()),
+                () -> assertEquals("tmp/uploads/xyz.jpg", unixRef.getPath()),
+                () -> assertTrue(unixRef.getParameters().isEmpty())
+        );
+
+        final ResourceLocation winRef = new ResourceLocation("file://C:/Windows/Temp/uploads/xyz.jpg");
+
+        assertAll(
+                () -> assertEquals("file", winRef.getProtocol()),
+                () -> assertEquals("C:", winRef.getVolumeName()),
+                () -> assertEquals("Windows/Temp/uploads/xyz.jpg", winRef.getPath()),
+                () -> assertTrue(winRef.getParameters().isEmpty())
         );
     }
 
     @Test
     public void parseUriWithParams() {
-        ResourceLocation ref = new ResourceLocation("file://tmp/uploads/xyz.jpg?createAt=2019-01-01&changedAt=2019-01-02");
+        final ResourceLocation unixRef = new ResourceLocation("file:///tmp/uploads/xyz.jpg?createAt=2019-01-01&changedAt=2019-01-02");
 
         assertAll(
-                () -> assertEquals(ref.getProtocol(), "file"),
-                () -> assertEquals(ref.getVolumeName(), "tmp"),
-                () -> assertEquals(ref.getPath(), "uploads/xyz.jpg"),
-                () -> assertEquals(ref.getParameters().get("createAt"), "2019-01-01"),
-                () -> assertEquals(ref.getParameters().get("changedAt"), "2019-01-02")
+                () -> assertEquals("file", unixRef.getProtocol()),
+                () -> assertEquals("", unixRef.getVolumeName()),
+                () -> assertEquals("tmp/uploads/xyz.jpg", unixRef.getPath()),
+                () -> assertEquals("2019-01-01", unixRef.getParameters().get("createAt")),
+                () -> assertEquals("2019-01-02", unixRef.getParameters().get("changedAt"))
+        );
+
+        final ResourceLocation winRef = new ResourceLocation("file://C:/Windows/Temp/uploads/xyz.jpg?createAt=2019-01-01&changedAt=2019-01-02");
+
+        assertAll(
+                () -> assertEquals("file", winRef.getProtocol()),
+                () -> assertEquals("C:", winRef.getVolumeName()),
+                () -> assertEquals("Windows/Temp/uploads/xyz.jpg", winRef.getPath()),
+                () -> assertEquals("2019-01-01", winRef.getParameters().get("createAt")),
+                () -> assertEquals("2019-01-02", winRef.getParameters().get("changedAt"))
         );
     }
 
     @Test
     public void parseUriWithEncodedParams() {
-        ResourceLocation ref = new ResourceLocation("file://tmp/uploads/xyz.jpg?description=Temp%20image&lang=portugu%C3%AAs");
+        final ResourceLocation unixRef = new ResourceLocation("file:///tmp/uploads/xyz.jpg?description=Temp%20image&lang=portugu%C3%AAs");
 
         assertAll(
-                () -> assertEquals(ref.getProtocol(), "file"),
-                () -> assertEquals(ref.getVolumeName(), "tmp"),
-                () -> assertEquals(ref.getPath(), "uploads/xyz.jpg"),
-                () -> assertEquals(ref.getParameters().get("description"), "Temp image"),
-                () -> assertEquals(ref.getParameters().get("lang"), "português")
+                () -> assertEquals("file", unixRef.getProtocol()),
+                () -> assertEquals("", unixRef.getVolumeName()),
+                () -> assertEquals("tmp/uploads/xyz.jpg", unixRef.getPath()),
+                () -> assertEquals("Temp image", unixRef.getParameters().get("description")),
+                () -> assertEquals("português", unixRef.getParameters().get("lang"))
+        );
+
+        final ResourceLocation winRef = new ResourceLocation("file://C:/Windows/Temp/uploads/xyz.jpg?description=Temp%20image&lang=portugu%C3%AAs");
+
+        assertAll(
+                () -> assertEquals("file", winRef.getProtocol()),
+                () -> assertEquals("C:", winRef.getVolumeName()),
+                () -> assertEquals("Windows/Temp/uploads/xyz.jpg", winRef.getPath()),
+                () -> assertEquals("Temp image", winRef.getParameters().get("description")),
+                () -> assertEquals("português", winRef.getParameters().get("lang"))
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource(delimiter = ';', value = {
+        "file:///tmp/tests/report.txt; file; ''; tmp/tests/report.txt",
+        "file://C:/Windows/Temp/tests/report.txt; file; C:; Windows/Temp/tests/report.txt",
+        "tmp://tests/report.txt; tmp; tests; report.txt",
+        "local://myapp/reports/march2019.pdf; local; myapp; reports/march2019.pdf",
+        "gs://app-images/profile/harry-potter.jpg; gs; app-images; profile/harry-potter.jpg",
+        "s3://app-images/profile/harry-potter.jpg; s3; app-images; profile/harry-potter.jpg"
+    })
+    public void checkUriFormats(String url, String protocol, String volumeName, String path) {
+        final ResourceLocation ref = new ResourceLocation(url);
+
+        assertAll(
+                () -> assertEquals(url, ref.toString()),
+                () -> assertEquals(protocol, ref.getProtocol()),
+                () -> assertEquals(volumeName, ref.getVolumeName()),
+                () -> assertEquals(path, ref.getPath())
         );
     }
 
